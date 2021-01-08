f53decfbbc8c9a7196a15342b569a28116c38234
Supakrit <supakrit@Supakrits-MacBook-Air.local>
2021-01-08 21:55:36 +0700

    Evaluate Email

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 var time = function(){
     document.getElementById('placeholder').innerHTML = Date();
 }
+
+var evaluateEmail = function(){
+    var email = document.getElementById('input-box').value;
+    document.getElementById('placeholder').innerHTML = evaluateEmailFunction(email);
+}
+
+var evaluateEmailFunction = function(e){
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(mailformat.test(e)){
+        return "Match";
+    }
+    return "Do not";
+}
